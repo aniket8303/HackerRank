c1c87deb8b78fea27d863b0b78eb2cff52d901f3
Aniket Parekar <you@example.com>
2026-02-09 18:28:52 +0530

    1st problem

---
+++ Java/Welcome to Java!/Solution.java
@@ -0,0 +1,17 @@
+// Problem: Welcome to Java!
+// Platform: HackerRank
+// Difficulty: Easy
+// Date: 09-02-2026
+// Link: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
